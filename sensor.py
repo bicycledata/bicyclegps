ab95cb0984c6c7337bcd5a445e4f39c06fa4995b
Lennart Ochel <lennart.ochel@outlook.com>
2025-05-16 20:30:03 +0200

    Fix worker_main

--- sensor.py
+++ sensor.py
@@ -18,37 +18,39 @@ class BicycleGPS(BicycleSensor):
     self.data_buffer.append(f'{str(time.time())}, {self._gps_time}, {self._latitude}, {self._longitude}, {self._altitude}')
 
   def worker_main(self):
-    self._gps_time = None
-    self._latitude = None
-    self._longitude = None
-    self._altitude = None
-    with serial.Serial('/dev/serial0', baudrate=9600, parity=PARITY_NONE, bytesize=EIGHTBITS, stopbits=STOPBITS_ONE) as ser:
-      ser.readline() # trash first line
+    def worker():
+      self._gps_time = None
+      self._latitude = None
+      self._longitude = None
+      self._altitude = None
+      with serial.Serial('/dev/serial0', baudrate=9600, parity=PARITY_NONE, bytesize=EIGHTBITS, stopbits=STOPBITS_ONE) as ser:
+        ser.readline() # trash first line
 
-      while self.alive:
-        try:
-          sentence = NMEA0183.bytes_to_sentence(ser.readline())
-        except Exception as e:
-          pass
-        except KeyboardInterrupt:
-          pass
-        else:
-          if sentence.topic == b'RMC':
-            try:
-              rmc = NMEA0183.RMC(sentence)
-            except Exception:
-              pass
-            else:
-              self._gps_time = rmc.time
-              self._latitude = rmc.latitude
-              self._longitude = rmc.longitude
-          elif sentence.topic == b'GGA':
-            try:
-              gga = NMEA0183.GGA(sentence)
-            except Exception:
-              pass
-            else:
-              self._altitude = gga.altitude
+        while self.alive:
+          try:
+            sentence = NMEA0183.bytes_to_sentence(ser.readline())
+          except Exception as e:
+            pass
+          except KeyboardInterrupt:
+            pass
+          else:
+            if sentence.topic == b'RMC':
+              try:
+                rmc = NMEA0183.RMC(sentence)
+              except Exception:
+                pass
+              else:
+                self._gps_time = rmc.time
+                self._latitude = rmc.latitude
+                self._longitude = rmc.longitude
+            elif sentence.topic == b'GGA':
+              try:
+                gga = NMEA0183.GGA(sentence)
+              except Exception:
+                pass
+              else:
+                self._altitude = gga.altitude
+    return worker
 
 
 if __name__ == '__main__':
