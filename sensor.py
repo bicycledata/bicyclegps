fe2feb866c00a4a321800528b545d8ca7f873466
Lennart Ochel <lennart.ochel@outlook.com>
2025-10-03 10:36:02 +0200

    bicyclegps for api-v2

--- sensor.py
+++ sensor.py
@@ -1,72 +1,48 @@
-#!/usr/bin/env python3
+import traceback
+from multiprocessing.connection import Connection
 
-import argparse
-import time
-
+import pynmea2
 import serial
 from serial.serialutil import EIGHTBITS, PARITY_NONE, STOPBITS_ONE
 
-import NMEA0183
-from BicycleSensor import BicycleSensor, configure
+from bicycleinit.BicycleSensor import BicycleSensor
 
 
-class BicycleGPS(BicycleSensor):
-  def write_header(self):
-    self.write_to_file('time, gps_time, latitude, longitude, altitude')
+def parse_nmea_sentence(sentence):
+  try:
+    msg = pynmea2.parse(sentence)
+    # Only process RMC sentences
+    if msg.sentence_type == 'RMC' and msg.lat_dir+msg.lon_dir in ['NE', 'NW', 'SE', 'SW']:
+      return msg.latitude, msg.longitude, msg.lat_dir+msg.lon_dir
+  except pynmea2.nmea.ParseError:
+      pass
+  return None
 
-  def write_measurement(self):
-    self.write_to_file(f'{str(time.time())}, {self._gps_time}, {self._latitude}, {self._longitude}, {self._altitude}')
+def main(bicycleinit: Connection, name: str, args: dict):
+  sensor = BicycleSensor(bicycleinit, name, args)
 
-  def worker_main(self):
-    self._gps_time = None
-    self._latitude = None
-    self._longitude = None
-    self._altitude = None
-    with serial.Serial('/dev/serial0', baudrate=9600, parity=PARITY_NONE, bytesize=EIGHTBITS, stopbits=STOPBITS_ONE) as ser:
-      ser.readline() # trash first line
+  port = args.get('port', '/dev/ttyACM0')
+  try:
+    ser = serial.Serial(port, baudrate=9600, parity=PARITY_NONE, bytesize=EIGHTBITS, stopbits=STOPBITS_ONE, timeout=5.0)
+  except serial.SerialException as e:
+    sensor.send_msg(f'Error opening serial port {port}: {e}')
+    return
 
-      while self._alive:
-        try:
-          sentence = NMEA0183.bytes_to_sentence(ser.readline())
-        except Exception as e:
-          pass
-        except KeyboardInterrupt:
-          pass
-        else:
-          if sentence.topic == b'RMC':
-            try:
-              rmc = NMEA0183.RMC(sentence)
-            except Exception:
-              pass
-            else:
-              self._gps_time = rmc.time
-              self._latitude = rmc.latitude
-              self._longitude = rmc.longitude
-          elif sentence.topic == b'GGA':
-            try:
-              gga = NMEA0183.GGA(sentence)
-            except Exception:
-              pass
-            else:
-              self._altitude = gga.altitude
+  sensor.write_header(['latitude', 'longitude', 'dir'])
+  try:
+    while True:
+      line = ser.readline().decode('ascii', errors='replace').strip()
+      gps_data = parse_nmea_sentence(line)
+      if gps_data is not None:
+        sensor.write_measurement(list(gps_data))
+  except KeyboardInterrupt:
+    pass
+  except Exception as e:
+    sensor.send_msg({'type': 'log', 'level': 'error', 'msg': str(e)})
+    sensor.send_msg({'type': 'log', 'level': 'error', 'msg': traceback.format_exc()})
+  finally:
+    ser.close()
+    sensor.shutdown()
 
-
-if __name__ == '__main__':
-  PARSER = argparse.ArgumentParser(
-    description='Sensor Template',
-    allow_abbrev=False,
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter
-  )
-  PARSER.add_argument('--hash', type=str, required=True, help='[required] hash of the device')
-  PARSER.add_argument('--name', type=str, required=True, help='[required] name of the sensor')
-  PARSER.add_argument('--loglevel', type=str, default='INFO', help='Set the logging level (e.g., DEBUG, INFO, WARNING)')
-  PARSER.add_argument('--measurement-frequency', type=float, default=1.0, help='Frequency of sensor measurements in 1/s')
-  PARSER.add_argument('--stdout', action='store_true', help='Enables logging to stdout')
-  PARSER.add_argument('--upload-interval', type=float, default=300.0, help='Interval between uploads in seconds')
-  ARGS = PARSER.parse_args()
-
-  # Configure logging
-  configure('bicyclegps.log', stdout=ARGS.stdout, rotating=True, loglevel=ARGS.loglevel)
-
-  sensor = BicycleGPS(ARGS.name, ARGS.hash, ARGS.measurement_frequency, ARGS.upload_interval, use_worker_thread=True)
-  sensor.main()
+if __name__ == "__main__":
+  main(None, "bicyclegps", {'port': '/dev/ttyACM0'})
