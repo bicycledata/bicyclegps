bf0c0d1f107c7ee395deb7df8ff1a6aafd771929
Lennart Ochel <lennart.ochel@vti.se>
2025-10-30 12:35:19 +0100

    Read GLL messages instead of RMC

--- sensor.py
+++ sensor.py
@@ -11,9 +11,9 @@ from bicycleinit.BicycleSensor import BicycleSensor
 def parse_nmea_sentence(sentence):
   try:
     msg = pynmea2.parse(sentence)
-    # Only process RMC sentences
-    if msg.sentence_type == 'RMC' and msg.lat_dir+msg.lon_dir in ['NE', 'NW', 'SE', 'SW']:
-      return msg.latitude, msg.longitude, msg.lat_dir+msg.lon_dir
+    # Only process GLL sentences
+    if msg.sentence_type == 'GLL' and msg.status == 'A':
+      return msg.latitude, msg.longitude
   except pynmea2.nmea.ParseError:
       pass
   return None
@@ -24,16 +24,16 @@ def main(bicycleinit: Connection, name: str, args: dict):
   port = args.get('port', '/dev/ttyACM0')
 
   try:
-    ser = serial.Serial(port, baudrate=4800, parity=PARITY_NONE, bytesize=EIGHTBITS, stopbits=STOPBITS_ONE, timeout=5.0)
+    ser = serial.Serial(port, baudrate=9600, parity=PARITY_NONE, bytesize=EIGHTBITS, stopbits=STOPBITS_ONE, timeout=5.0)
   except serial.SerialException as e:
     sensor.send_msg(f'Error opening serial port {port}: {e}')
     return
 
-  sensor.write_header(['latitude', 'longitude', 'dir'])
+  sensor.write_header(['latitude', 'longitude'])
 
   try:
     while True:
-      line = ser.readline().decode('ascii', errors='replace').strip()
+      line = ser.readline().decode('ascii', errors='ignore').strip()
       gps_data = parse_nmea_sentence(line)
 
       if gps_data is not None:
